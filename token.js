bd4529b32bdfeb99a982be44f354bf17ae29cbfe
Tulir Asokan <tulir@maunium.net>
2020-01-05 18:09:44 +0200

    Add chrome support

--- token.js
+++ token.js
@@ -1,4 +1,9 @@
 if (localStorage.mautrixLoginSite === window.location.pathname) {
-	browser.runtime.sendMessage({token: window.location.hash.substr(1)})
-		.then(() => console.info("Login token eaten by Mautrix Cookie Monster"))
+	if (browser) {
+		browser.runtime.sendMessage({token: window.location.hash.substr(1)})
+			.then(() => console.info("Login token eaten by Mautrix Cookie Monster"))
+	} else if (chrome) {
+		chrome.runtime.sendMessage({token: window.location.hash.substr(1)},
+			() => console.info("Login token eaten by Mautrix Cookie Monster"))
+	}
 }
